48a0917c653591f21c332eb7994e1d8e5e91ffbf
Catherine Borsting <cborsting@Catherines-MacBook-Pro.local>
2020-06-10 18:54:10 -0700

    Fixed database issue

--- api/app.py
+++ api/app.py
@@ -1,5 +1,5 @@
-from flask import request, make_response, Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
+from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
